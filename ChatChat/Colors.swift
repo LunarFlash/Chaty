fdfcb05e24931baab62adb3df375ca5eb2cce554
Terry Wang <terry.wang@accenture.com>
2016-02-13 17:11:44 -0500

    changed theme

---
+++ ChatChat/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  ChatChat
+//
+//  Created by Terry Wang on 2/13/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//  Color inspirations https://dribbble.com/shots/2091055-Get-DO-UI-Kit-for-Photoshop-Sketch
+
+import UIKit
+
+struct Colors {
+    
+    static let mainTint = UIColor(red: 178.0/255.0, green: 120.0/255.0, blue: 243.0/255.0, alpha: 1)
+    
+    
+}
