0f1ee46f441116a03930e84848a6921c22a059c9
Yi Wang <yiwang1982@gmail.com>
2016-02-12 19:38:00 -0500

    authentication -  check

---
+++ ChatChat/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ChatChat
+//
+//  Created by Terry Wang on 2/12/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://chatyi.firebaseIO.com/"
--- ChatChat/LoginViewController.swift
+++ ChatChat/LoginViewController.swift
@@ -1,35 +1,31 @@
-/*
-* Copyright (c) 2015 Razeware LLC
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+// Yi
 
 import UIKit
+import Firebase
+
 class LoginViewController: UIViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
-  @IBAction func loginDidTouch(sender: AnyObject) {
-
-  }
-  
+    
+    // MARK: Properties
+    let ref: Firebase  = Firebase(url: BASE_URL)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginDidTouch(sender: AnyObject) {
+        
+        ref.authAnonymouslyWithCompletionBlock { (error, authData) -> Void in
+            if error != nil {  // Check for an authentication error.
+                print(error.description)
+                return
+            }
+            // Inside of the closure, trigger the segue to move to ChatViewController.
+            print(self.ref.authData)
+            self.performSegueWithIdentifier("LoginToChat", sender: self)
+        }
+        
+    }
+    
 }
 
