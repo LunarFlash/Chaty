a1c53763d2c5f00704387a80cfa32a4702827468
Yi Wang <yiwang1982@gmail.com>
2016-02-12 19:45:48 -0500

    initial setup for chatvc using anonymous login info from prepareforsegue

--- ChatChat/LoginViewController.swift
+++ ChatChat/LoginViewController.swift
@@ -13,6 +13,18 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        super.prepareForSegue(segue, sender: sender)
+        
+        let navVC = segue.destinationViewController as! UINavigationController
+        let chatVC = navVC.viewControllers.first as! ChatViewController
+        
+        chatVC.senderId = ref.authData.uid // Assign the local user’s ID to chatVc.senderId; this is the local ID that JSQMessagesViewController uses to coordinate messages.
+        chatVC.senderDisplayName = "" // chatVc.senderDisplayName is set to empty string, since this is an anonymous chat room.
+    }
+    
+    
+    
     @IBAction func loginDidTouch(sender: AnyObject) {
         
         ref.authAnonymouslyWithCompletionBlock { (error, authData) -> Void in
