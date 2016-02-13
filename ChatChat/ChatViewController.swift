647413d885676acefc9ab11d5547bf523a416377
Yi Wang <yiwang1982@gmail.com>
2016-02-12 20:27:11 -0500

    setup bubbles

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -5,18 +5,47 @@ import Firebase
 import JSQMessagesViewController
 
 class ChatViewController: JSQMessagesViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    title = "Chatyi"
-  }
-  
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-  }
-  
-  override func viewDidDisappear(animated: Bool) {
-    super.viewDidDisappear(animated)
-  }
-  
+    
+    // MARK: Properties
+    var messages = [JSQMessage]() // messages is an array to store the various instances of JSQMessage
+    
+    
+    var outgoingBubbleImageView: JSQMessagesBubbleImage!
+    var incomingBubbleImageView: JSQMessagesBubbleImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Chatyi"
+        setupBubbles()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    
+    private func setupBubbles() {
+        // JSQMessagesBubbleImageFactory has methods that create the images for the chat bubbles. There’s even a category provided by JSQMessagesViewController that creates the message bubble colors used in the native Messages app.
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingBubbleImageView = factory.outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleBlueColor())
+        incomingBubbleImageView = factory.incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleLightGrayColor())
+    }
+    
+
+    // MARK: JSQMessagesCollectionView Datasource    
+    override func collectionView(collectionView: UICollectionView,
+        numberOfItemsInSection section: Int) -> Int {
+            return messages.count
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!,
+        messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+            return messages[indexPath.item]
+    }
 }
+
+
