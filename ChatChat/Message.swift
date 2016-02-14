6e7bef0ae1ab7f3eda2b61557ed7a448802744e5
Yi Wang <yiwang1982@gmail.com>
2016-02-13 22:38:38 -0500

    created model struct

---
+++ ChatChat/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Chat
+//
+//  Created by Yi Wang on 2/13/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+struct Message {
+  
+    let text: String!
+    let senderId: String!
+    
+    
+}
